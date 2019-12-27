2abe026e7ad919049333a894a5cc5a4cfab9ce1c
aben-bel <abenezer.sbelay@gmail.com>
2019-12-27 15:31:46 +0300

    Updates

---
+++ precache-manifest.6c4e8094646c01373b32c1cea37f087d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43ee316fc9644254367bbc5da4a93973",
+    "url": "/dsc-post/index.html"
+  },
+  {
+    "revision": "738f048f54a9e517858a",
+    "url": "/dsc-post/static/css/main.54f47931.chunk.css"
+  },
+  {
+    "revision": "4d53f27b2a4378665cba",
+    "url": "/dsc-post/static/js/2.dfdea07d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dsc-post/static/js/2.dfdea07d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "738f048f54a9e517858a",
+    "url": "/dsc-post/static/js/main.0a72f063.chunk.js"
+  },
+  {
+    "revision": "92740061d8261c946e79",
+    "url": "/dsc-post/static/js/runtime-main.ca36172e.js"
+  }
+]);
